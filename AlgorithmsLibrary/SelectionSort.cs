b719cee545e548ae1fcb8a6476dc6c6f7f9f29c0
alexandr <sashkaageev@gmail.com>
2021-01-07 23:11:34 +0300

    Added Selection Sort

---
+++ AlgorithmsLibrary/SelectionSort.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsLibrary
+{
+    public class SelectionSort<T> : AlgorithmBase<T> where T : IComparable
+    {
+        public SelectionSort(IEnumerable<T> items) : base(items) { }
+        public SelectionSort() { }
+
+        public override void Sort()
+        {
+            var minIndex = 0;
+
+            for (var i = 0; i < Items.Count - 1; i++)
+            {
+                minIndex = i;
+
+                for (var j = i + 1; j < Items.Count; j++)
+                {
+                    if (Items[j].CompareTo(Items[minIndex]) == -1)
+                    {
+                        minIndex = j;
+                        
+                    }
+
+                    if (i != minIndex)
+                    {
+                        Swap(i, minIndex);
+                    }
+                }
+            }
+        }
+    }
+}
